0f655f491181a60e9fabb4a5037f0848dea26b28
dalvi.shekhar <dalvi.shekhar@gmail.com>
2021-07-05 11:22:58 +0530

    added prod/dev wise settings

--- ShopBridge.WebAPI/Program.cs
+++ ShopBridge.WebAPI/Program.cs
@@ -11,13 +11,21 @@ namespace ShopBridge.WebAPI
 {
     public class Program
     {
+        static string env;
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            env = config.GetSection("Env").Value;
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args) 
+                .ConfigureAppConfiguration((a, config) =>
+                {
+                    config.AddJsonFile("appsettings.json");
+                    config.AddJsonFile($"appsettings.{env}.json");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
